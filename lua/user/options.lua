c2558bb1399d0850a3f30a229de490d9765ed253
Cary W Reams <Cary.Reams@CoreClarity.net>
2022-04-30 08:41:26 -0500

    adds fonts from inspiration repo removes plugins from README in favor of repo wiki comments options; minor changes comments keymaps; minor changes

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -4,7 +4,7 @@ local options = {
   cmdheight = 2,                           -- more space in the neovim command line for displaying messages
   completeopt = { "menuone", "noselect" }, -- mostly just for cmp
   conceallevel = 0,                        -- so that `` is visible in markdown files
-  colorcolumn = "80",
+  colorcolumn = "80",                      -- column number to highlight
   -- colorcolumn = "120",
   fileencoding = "utf-8",                  -- the encoding written to a file
   hlsearch = true,                         -- highlight all matches on previous search pattern
@@ -28,25 +28,43 @@ local options = {
   tabstop = 2,                             -- insert 2 spaces for a tab
   cursorline = true,                       -- highlight the current line
   number = true,                           -- set numbered lines
-  laststatus = 3,
-  relativenumber = false,                  -- set relative numbered lines
+  laststatus = 2,                          -- setting for last window having a status line 
+  relativenumber = true,                  -- set relative numbered lines
   numberwidth = 4,                         -- set number column width to 2 {default 4}
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
   wrap = false,                            -- display lines as one long line
-  scrolloff = 8,                           -- is one of my fav
-  sidescrolloff = 8,
+  scrolloff = 8,                           -- minimum number of screen lines above and below cursor
+  sidescrolloff = 8,                       -- with nowrap, the min number of columns to keep left and right of the cursor
   guifont = "monospace:h17",               -- the font used in graphical neovim applications
 
 }
+
+-- netrw is for editing files on remote machine
+-- these settings prevent loading of netrw
+-- see netrw-noload documentation
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+
+-- set of flags to improve UX and informational messages from nvim 
 vim.opt.shortmess:append "c"
 
+-- set the options from the list above
 for k, v in pairs(options) do
   vim.opt[k] = v
 end
 
+-- permits cursor movement to wrap end/beginning  of line
 vim.cmd "set whichwrap+=<,>,[,],h,l"
+
+-- keywords used in search: not sure how I'd use this... TODO
 vim.cmd [[set iskeyword+=-]]
+
+-- influences automatic formatting... TODO
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+
+-- migrating from my .vimrc
+
+-- vim.cmd "set ignorecase" 
+-- @TODO: weird: finds all caps if search for lowercase; not lc if search for all caps
+
